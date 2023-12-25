ea5b4d0c41e6c5d5e4f1fc6f8f7c31c083d28571
Zaid Nadaf <zaidnadaf0@gmail.com>
2023-12-25 19:45:19 +0530

    skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,88 +1,380 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
-    <div id="skills" class="p-4  border-2 h-full border-gray-200 border-dashed rounded-lg dark:border-gray-700">  
-    <div class="py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-12">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-900">Skills</h1>
-       
-
+    <div
+      id="skills"
+      class="p-4  border-2 h-full border-gray-200 border-dashed rounded-lg dark:border-gray-700"
+    >
+      <div class="py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-12">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-900">
+          Skills
+        </h1>
 
         <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    
-    <div class="flex flex-wrap -m-2">
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Data Analysis</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Analytical Skills</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Business Analysis</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Intellectual property, Patents, Power Electronics, UPS</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Patentability</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Intellectual Property</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Power Electronics</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">FTO Analysis</h2>
-          </div>
-        </div>
-      </div>  
-      <div class="p-2 lg:w-1/4 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">Freedom to Operate (FTO)</h2>
-          </div>
-        </div>
-      </div>  
+          <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap -m-2 cursor-pointer">
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Analytical Skills
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Intellectual property
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Business Analysis
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Intellectual Property
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+          "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Power Electronics
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+        "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      FTO Analysis
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+      "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Freedom to Operate (FTO)
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+    "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Competitive Analysis
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+  "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Competitive Intelligence
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+"
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Due Diligence
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Innovation Management
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+          "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Patent Searching
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+        "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Prior Art Search
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Six Sigma
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Electrical Engineering
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Electronics
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Energy</h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Joint Ventures
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Licensing
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">R&D</h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Technology Transfer
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Mergers & Acquisitions
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">IPIP</h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Patent Law
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Patent Litigation
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Patent Prosecution
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Product Development
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Prosecution
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Renewable Energy
+                    </h2>
+                  </div>
+                </div>
+              </div>
 
+              <div
+                class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
+            "
+              >
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      UPSIntellectual property
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
+  );
+};
 
-  </div>
-</section>
-        </div>
-        </div>
-  )
-}
-
-export default Skills
+export default Skills;
