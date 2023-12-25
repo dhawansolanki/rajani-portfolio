6cffc160be28d8ca3e510f6ec113c245d849f6ec
Dhawan Solanki <dhawansolanki2103@gmail.com>
2023-12-25 19:47:26 +0530

    Experience

--- src/Experience.js
+++ src/Experience.js
@@ -5,16 +5,81 @@ const Experience = () => {
     <div id="experience" class="p-4 border-2 h-full border-gray-200 border-dashed rounded-lg dark:border-gray-700">  
     <div class="py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-12">
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-900">Experience</h1>
-        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl   dark:text-gray-700 text-left">Connecting dots with integrated Analytics with Patent, Market, and Literature data to serve the business strategy. 
-
-Driving Innovation culture through IP Training, e-Learning modules, Idea Generation/Ideation, World IP Day events, and by having Strategic partnerships with R&D Teams.</p>
-<p>End to End IP support with Effective, Expert patent Searching techniques, Information retrieval, and data handling, 
-</p>
-<p>Aspiring board member for IP Strategy covering Patentability Analysis, Potential Risk Assessments, Infringement, FTO Analysis/ IP Clearance Analysis, IP Risk Mitigation Strategy, Competitor Intelligence, 3rd party Portfolio analysis, Patent-to Product-Mapping, Intellectual property/ Technology landscaping, Secondary Market Analytics, SWOT, White Space Analysis, Data Migration, M&D Support, Licensing, Monetization, Patent portfolio Pruning, 
-    <p>Technologies: Power electronics, UnInterruptible Power Supply, Motors, Drives, High Voltage Engineering, Electric Arc, Smart Buildings, Smart Power, Power Supplies, Converters, Breakers, Equipment- MCC/Motor Control Centers, 
-    </p>
-    DataScience: Python, EDA, Data Mining, Predictive, Modeling, AI/ML, Artificial Intelligence/Machine Learning, Time Series, Data Visualization, Tableau, Market Retail Analysis, Finance and Risk Analytics, KNIME, Neural Networks, ANN, Regression, Predictive Modeling,
-</p>
+        <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto flex flex-wrap">
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 lg:w-full md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <img src="/great_lake_institute_of_management.jpeg" alt="great_lake_institute_of_management" />
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Patent Officer ABB eMobility</h2>
+            <strong><p>Jul 2020 - Aug 2021</p></strong>
+            <br/>
+            <p class="leading-relaxed text-base">Postgraduate Degree, Data Science and Business Analytics</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-full md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <img src="/great_lake_institute_of_management.jpeg" alt="great_lake_institute_of_management" />
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">ABB</h2>
+            <strong><p>Jul 2020 - Aug 2021</p></strong>
+            <br/>
+            <p class="leading-relaxed text-base">Postgraduate Degree, Data Science and Business Analytics</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-full md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <img src="/great_lake_institute_of_management.jpeg" alt="great_lake_institute_of_management" />
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Lead Intellectual Property Business Analyst</h2>
+            <strong><p>Jul 2020 - Aug 2021</p></strong>
+            <br/>
+            <p class="leading-relaxed text-base">Postgraduate Degree, Data Science and Business Analytics</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-full md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <img src="/bangalore_university_Logo.webp" alt="Bangalore University Logo" />
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">GE Energy Connections</h2>
+            <strong><p>2005 - 2007</p></strong>
+            <br/>
+            <p class="leading-relaxed text-base">M.E, Power Electronics</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-full md:w-full">
+        <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <img src='/geglobalresearch_logo.jpeg' alt='geglobalresearch_logo'/>
+          </div>
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Intern at GE Global Research Center - High Voltage Lab</h2>
+            <strong><p>Oct 2006 - Aug 2007</p></strong>
+            <strong><p>GE Global Research Center, JFWTC Bangalore</p></strong>
+            <br/>
+            <p class="leading-relaxed text-base">• Performed High voltage arc-related experiments, analysis which involved capturing High-speed images, time coordination, and interpretation. </p>
+            <p class="leading-relaxed text-base">• Collaborated with Chief Science of the High Voltage lab to design and capture Arc propagations in different electrode gaps and using different Power Sources. </p>
+            <p class="leading-relaxed text-base">• Arc Worked on High Voltage Low current arc and High current generator</p>
+            <p class="leading-relaxed text-base">• This work led to the first-time understanding of arc propagation with 3 publications: Electric society of America – 2007 Annual meeting, 1-TISCAT, and 1-JFWTC Journal. Journal paper titled “High Voltage and Low Current Arc Propagation” published in Electrostatic Society of America (ESA) for student paper conference.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
         </div>
     </div>
   )
