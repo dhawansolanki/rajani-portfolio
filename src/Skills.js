ec8a339f9a002c9bbc92e03b395adc1474f3036b
Zaid Nadaf <zaidnadaf0@gmail.com>
2023-12-25 21:20:11 +0530

    experience

--- src/Skills.js
+++ src/Skills.js
@@ -4,7 +4,7 @@ const Skills = () => {
   return (
     <div
       id="skills"
-      class="p-4  border-2 h-full border-gray-200 border-dashed rounded-lg dark:border-gray-700"
+      class="p-4 border-2 h-full border-gray-200 border-dashed rounded-lg dark:border-gray-700"
     >
       <div class="py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-12">
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-900">
@@ -12,7 +12,7 @@ const Skills = () => {
         </h1>
 
         <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
+          <div class="container px-5  md:py-8 py-24 mx-auto">
             <div class="flex flex-wrap -m-2 cursor-pointer">
               <div
                 class="p-2 lg:w-1/4 md:w-1/2 w-full hover:shadow-gray-900 hover:shadow-lg transition duration-300 ease-in-out 
